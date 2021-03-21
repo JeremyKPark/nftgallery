b065423a8d7c0b9c32cc27efe277d8810246ee70
Jeremy K <jeremyk@park-agency.com>
2021-03-21 13:04:13 -0400

    updates

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
       <div className={styles.header__logo}>
         <Link href="/">
           <a>
-            <img src="/logo_orb.png" alt="Zora" />
+            <img src="/logo_orb.png" alt="Zora" stlye="width:400px;height:100px;">
           </a>
         </Link>
       </div>
